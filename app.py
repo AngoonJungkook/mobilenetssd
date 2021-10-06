cf9071a7d4d652a84005d75c8b8aae54b3e9ea26
AngoonJungkook <91891610+AngoonJungkook@users.noreply.github.com>
2021-10-06 14:37:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,8 +26,7 @@ UPLOAD_FOLDER ='static/uploads/'
 DOWNLOAD_FOLDER = 'static/downloads/'
 ALLOWED_EXTENSIONS = {'jpg', 'png','.jpeg'}
 
-lineaccesstoken = ''
-
+U26gtdGdHr2cECej2tv+nQrCWnZtOR5q3PHChH/yd1QkAxgnFt9E5p1JisoNcNXvYNsDenuAJFNy/OO8P9+hhRsHev3n/6GDeelMqYGpSfNMZ+ZbuGVRhgFizooGdelMqYGpSfNMZ+ZbuGVRhgFizo'
 line_bot_api = LineBotApi(lineaccesstoken)
 
 # APP CONFIGURATIONS
